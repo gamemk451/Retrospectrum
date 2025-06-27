37f4755d2d02a5bc1c2d414c0e1c96fc42c1eefb
GameMK <donuts4allyay@gmail.com>
2025-06-27 08:34:45 -0400

    Update game.js

--- game.js
+++ game.js
@@ -5,8 +5,8 @@ kaboom()
 loadSprite("fly", "fly.png")
 
 const Player = add([
-  sprite("fly")
-  pos(180, 20)
-  rotate(0)
-  anchor(center)
+  sprite("fly"),
+  pos(180, 20),
+  rotate(0),
+  anchor("center"),
 ])
