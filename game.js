5d430808fb87d742709cf3a9a84b5d6ea111c49d
GameMK <donuts4allyay@gmail.com>
2025-06-27 08:48:19 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ kaboom()
 
 //setGravity(2400)
 
+const MOVEMENT_SPEED = 240
+
 loadSprite("fly", "fly.png")
 
 const Player = add([
@@ -11,3 +13,7 @@ const Player = add([
   scale(4),
   anchor("center"),
 ])
+
+onKeyDown("right", () => {
+    bean.move(-SPEED, 0)
+})
